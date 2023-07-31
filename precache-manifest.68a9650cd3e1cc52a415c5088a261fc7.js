bce7b81b3c7b1c3086dad4ec21af12ee0e3c9006
arnaudbrice <mephisteur@gmail.com>
2023-07-31 19:31:50 +0200

    Updates

--- precache-manifest.68a9650cd3e1cc52a415c5088a261fc7.js
+++ precache-manifest.68a9650cd3e1cc52a415c5088a261fc7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "e2b778f3dba251b2331a",
-    "url": "/robofriends_react/static/css/main.0636b85a.chunk.css"
+    "revision": "c49a0018e0c6c20ec34b",
+    "url": "/robofriends_react/static/css/main.c89c9479.chunk.css"
   },
   {
-    "revision": "e2b778f3dba251b2331a",
-    "url": "/robofriends_react/static/js/main.e2b778f3.chunk.js"
+    "revision": "c49a0018e0c6c20ec34b",
+    "url": "/robofriends_react/static/js/main.c49a0018.chunk.js"
   },
   {
     "revision": "c3777927de14cc17cd66",
@@ -24,27 +24,27 @@ self.__precacheManifest = [
     "url": "/robofriends_react/static/js/runtime~main.d1e5b7c6.js"
   },
   {
-    "revision": "0d6b19f13fb422b56d250a81230da78a",
-    "url": "/robofriends_react/static/media/Gerbera.0d6b19f1.woff2"
-  },
-  {
-    "revision": "54f95b5a0e99ed344d5e2c160fc6abfd",
-    "url": "/robofriends_react/static/media/Gerbera.54f95b5a.ttf"
-  },
-  {
     "revision": "9701a44cf0e1c1be56673fac1965a9ef",
     "url": "/robofriends_react/static/media/Gerbera.9701a44c.woff"
+  },
+  {
+    "revision": "0d6b19f13fb422b56d250a81230da78a",
+    "url": "/robofriends_react/static/media/Gerbera.0d6b19f1.woff2"
   },
   {
     "revision": "eb56299c74e732e79be8e3dd94700a8a",
     "url": "/robofriends_react/static/media/Gerbera.eb56299c.eot"
   },
   {
+    "revision": "54f95b5a0e99ed344d5e2c160fc6abfd",
+    "url": "/robofriends_react/static/media/Gerbera.54f95b5a.ttf"
+  },
+  {
     "revision": "632ad65363f8b4e7681bfa49388a3b66",
     "url": "/robofriends_react/static/media/ROBO.632ad653.woff"
   },
   {
-    "revision": "ba406f9b62c467b6e219683a634d00ed",
+    "revision": "a37a54fa23a06ee075e7756789179819",
     "url": "/robofriends_react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robofriends_react/precache-manifest.853433dcef33df4c3a8f3ddeb40dd5bd.js"
+  "/robofriends_react/precache-manifest.68a9650cd3e1cc52a415c5088a261fc7.js"
 );
 
 workbox.clientsClaim();
